2f21ea6101c2efe93ee643406476c80337095716
Ari Roffe <aroffe@mail.yu.edu>
2020-07-22 22:54:17 -0500

    optimizing using the moving window method

--- algorithms/LengthOfLongestSubstring.js
+++ algorithms/LengthOfLongestSubstring.js
@@ -3,27 +3,20 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    if(s.length == 1) return 1;
-    var mapOfString = new Array(52).fill(false);
-    
-    var max = 0;
-    var current_count = 0;
-    for(j = 0; j < s.length; j++){
-        chars = s.substring(j); //chard is really a substring
-        for(i = j; i < chars.length; i++){
-            if(mapOfString[chars.charCodeAt(i) - 65] == true){
-                mapOfString.fill(false);
-                mapOfString[chars.charCodeAt(i) - 65] = true;
-            
-                if(current_count > max){
-                    max = current_count;
-                }
-                current_count = 1;// start the count over
-            }
-            else{
-                mapOfString[chars.charCodeAt(i) - 65] = true;
-                current_count++;
-            }
+    let set = new Set();
+    let i = 0, j = 0, max = 0;
+    let current_count = 0;
+    while(i < s.length && j < s.length){
+        if(set.has(s[j])){
+            set.delete(s[i]);
+            i++;
+            current_count--;
+        }
+        else{
+            set.add(s[j]);
+            j++;
+            current_count++;
+            if(current_count > max) max = current_count;
         }
     }
     return max;
