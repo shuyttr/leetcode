6dcd6b6a39a93ce12eec5bfad5bb68cb82d13aaf
Ari Roffe <aroffe@mail.yu.edu>
2020-07-22 20:56:28 -0500

    Update LinkListAddition.java
    moved from leetcode website

--- algorithms/LinkListAddition.java
+++ algorithms/LinkListAddition.java
@@ -10,75 +10,35 @@
  */
 class LinkListAddition {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        
         ListNode result = new ListNode();
-        ListNode headResult = result;
-
-        ListNode current1 = l1;
-        ListNode current2 = l2;
-
+        ListNode headResult = result; // return this
+        
         int carry = 0;
-        int x;
-        int y;
-        do {
-            if(current1 == null){
-                x = 0;
-            } else{
-                x = current1.val;
+        
+        while(c1 != null || c2 != null){
+            int c1_val = (c1 != null) ? c1.val : 0;
+            int c2_val = (c2 != null) ? c2.val : 0;
+            
+            int sum = (c1_val + c2_val + carry) % 10;
+            carry = (c1_val + c2_val + carry) / 10;
+            
+            result.next = new ListNode(sum);
+            result = result.next;
+            if(c1 != null){
+                c1 = c1.next;
             }
-
-            if(current2 == null){
-                y = 0;
-            } else{
-                y = current2.val;
+            if(c2 != null){
+                c2 = c2.next;
             }
-            int addition = (x + y) % 10;
-            result.val = carry + addition;
-            carry = (x + y) / 10;
-            if(current1.next != null || current2.next != null){
-                result.next = new ListNode();
-                result = result.next;
-            }
-        } while(current1.next != null || current2.next != null);
-        return headResult;
-    }
-
-    static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
         }
-
-        ListNode(int val) {
-            this.val = val;
+        
+        if(carry > 0){
+            result.next = new ListNode(1);
         }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder stringBuilder = new StringBuilder();
-            ListNode current = this;
-            while(current != null){
-                stringBuilder.append(current.val + " -> ");
-            }
-            return stringBuilder.toString();
-        }
-    }
-
-    public static void main(String[] args) {
-        LinkListAddition addition = new LinkListAddition();
-
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
-
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
-        System.out.println(addition.addTwoNumbers(l1, l2));
+        
+        return headResult.next;
     }
 }
