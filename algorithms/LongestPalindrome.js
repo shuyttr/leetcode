299443c098ab0f1245d04f746147084e3969ad5b
Ari Roffe <aroffe@mail.yu.edu>
2020-07-24 16:41:48 -0500

    completed with hint

--- algorithms/LongestPalindrome.js
+++ algorithms/LongestPalindrome.js
@@ -6,21 +6,22 @@ var longestPalindrome = function(s) {
     // if odd amoung of chars, start from s.length/2
     // if even amount of chars, start from s.length/2 and (s.length/2)-1
     // make the "mid" every char in the string
-    for(i = 0; i < s.length; i++){
-        //one letter center
-        var odd = length_of_palendrome(s, i, i)
-        //two letter center
-        var odd = length_of_palendrome(s, i, i+1) 
+    
+    var length_of_palindrome = function(s, left, right){
+        while(left >= 0 && right < s.length && (s[left] == s[right])){
+            left--;
+            right++;
+        }
+        var result = s.slice(left+1, right);
+        return result;
     }
     
-    // a single char is a palindrome with itself
-    // move "right" and move "left", and compare if they equal,
-    // if not equal, then see if either is same as middle
-    
-    // if not equal, nor same as mid, then recursively make those the mid
+    var longest = '';
+    for(i = 0; i < s.length; i++){
+        var odd = length_of_palendrome(s, i, i);
+        var even = length_of_palendrome(s, i, i+1);
+        if(odd.length > longest.length) longest = odd;
+        if(even.length > longest.length) longest = even;
+    }
+    return longest;
 };
-
-function length_of_palendrome(s, left, right){
-    //while the string is not out of bounds, ON BOTH ENDS, and while left and right equal each other, continue moving out
-    while()
-}
