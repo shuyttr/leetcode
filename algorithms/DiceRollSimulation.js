e301d4e640edc97b6282f0a177091f6108a5fc13
Ari <aroffe@mail.yu.edu>
2020-07-26 08:55:02 -0500

    fixed the n>3 case with new array

--- algorithms/DiceRollSimulation.js
+++ algorithms/DiceRollSimulation.js
@@ -3,27 +3,31 @@
  * @param {number[]} rollMax
  * @return {number}
  */
-function complete(n, rollMax, sum){
-    if(n == 1){
+function complete(n, rollMax, consecutive, sum) {
+    var copy = rollMax.map((x) => x);
+    if (consecutive != null) rollMax[consecutive]--;
+    if (n == 1) {
         var count = 0;
-        for(i = 0; i < rollMax.length; i++){
-            if(rollMax[i] > 0){
+        for (i = 0; i < rollMax.length; i++) {
+            if (rollMax[i] > 0) {
                 count++;
             }
         }
         return count;
     }
-    for(j = 0; j < rollMax.length; j++){
-        if(rollMax[j] > 0){
+    for (j = 0; j < rollMax.length; j++) {
+        if (rollMax[j] > 0) {
             var arr = rollMax.map((x) => x);
-            arr[j]--;
-            sum += complete(n-1, arr);
+            sum += complete(n - 1, copy, j);
         }
     }
     return sum;
 }
 
 var dieSimulator = function(n, rollMax) {
-    var roll = complete(n, rollMax, 0);
+    let sum = 0;
+    var roll = complete(n, rollMax);
     return roll % (Math.pow(10, 9) + 7);
 };
+
+dieSimulator(3, [1, 1, 1, 2, 2, 3]);
