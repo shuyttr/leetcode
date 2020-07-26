8212ad8d636f7522d82b7e94adcca01242009527
Ari Roffe <aroffe@mail.yu.edu>
2020-07-25 23:36:37 -0500

    function invocation, correct + debugging 14/23

--- algorithms/DiceRollSimulation.js
+++ algorithms/DiceRollSimulation.js
@@ -3,24 +3,26 @@
  * @param {number[]} rollMax
  * @return {number}
  */
-var dieSimulator = function(n, rollMax) {
-    let sum = 0;
-    var complete = function(n, rollMax){
-        if(n == 1){
-            var count = 0;
-            for(i = 0; i < rollMax.length; i++){
-                if(rollMax[i] > 0){
-                    count++;
-                }
-            }
-            return count;
-        }
+function complete(n, rollMax, sum){
+    if(n == 1){
+        var count = 0;
         for(i = 0; i < rollMax.length; i++){
             if(rollMax[i] > 0){
-                var arr = rollMax.map((x) => x);
-                arr[i]--;
-                sum += complete(n-1, arr);
+                count++;
             }
         }
+        return count;
     }
+    for(i = 0; i < rollMax.length; i++){
+        if(rollMax[i] > 0){
+            var arr = rollMax.map((x) => x);
+            arr[i]--;
+            sum += complete(n-1, arr);
+        }
+    }
+    return sum;
+}
+
+var dieSimulator = function(n, rollMax) {
+    var roll = complete(n, rollMax, 0);
 };
