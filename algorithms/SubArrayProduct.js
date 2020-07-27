e32828d675f88d8d2375417b607449640e4e4a4e
Ari Roffe <aroffe@mail.yu.edu>
2020-07-26 22:47:14 -0500

    sliding window solution

--- algorithms/SubArrayProduct.js
+++ algorithms/SubArrayProduct.js
@@ -4,35 +4,14 @@
  * @return {number}
  */
 var numSubarrayProductLessThanK = function(nums, k) {
-    let i = 0, j = 0, total = 0;
-    for(var singles = 0; singles < nums.length; singles++){
-        if(nums[singles] < k){
-            total++;
-        }
+    if(k <= 1) return 0;
+    var product = 1;
+    var result = 0;
+    var left = 0;
+    for(var right = 0; right < nums.length; right++){
+        product *= nums[right];
+        while(product >= k) product /= nums[left++];
+        result += right - left + 1;
     }
-    
-    while(i < nums.length && j < nums.length){
-        if(i == j){
-            // a "single", which has been counted
-            j++;
-            continue;
-        }
-        var multiply = 1;
-        for(var p = i; p <= j; p++){
-            multiply *= nums[p];
-        }
-        if(multiply < k){
-            total++;
-            if(j + 1 != nums.length){
-                j++;
-            } else{
-                //stop j at end of array and continue moving up i
-                i++;
-            }
-        } else{
-            i++;
-        }
-    }
-    
-    return total;
+    return result;
 };
