1f6bac5965992abd6850fe080a0e51887bcae60f
Ari Roffe <aroffe@mail.yu.edu>
2020-08-02 18:20:57 -0500

    added initial logic

--- algorithms/StockBuySell.js
+++ algorithms/StockBuySell.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
+    var low = 0;
+    var high = 0;
+    var profit = 0;
+    while(high < prices.length){
+        if(prices[high] - prices[low] > profit){
+            profit = prices[high] - prices[low];
+        }
+        if(prices[high+1] < prices[low]){
+            low = high + 1;
+        }
+        high++;
+    }
 };
