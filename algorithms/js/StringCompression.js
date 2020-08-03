74b86c3735a8e3c654f79dbcc1610aa35b7dc947
Ari Roffe <aroffe@mail.yu.edu>
2020-08-02 20:59:54 -0500

    empty array returned, need fix

--- algorithms/js/StringCompression.js
+++ algorithms/js/StringCompression.js
@@ -3,5 +3,20 @@
  * @return {number}
  */
 var compress = function(chars) {
-    
+    var lo = 0;
+    var hi = 0;
+    var result = [];
+    var count = 0;
+    var i = 0;
+    while(hi < chars.length){
+        while(chars[lo] === chars[hi]){
+            count++;
+            hi++;
+        }
+        result[++i] = chars[lo];
+        result[i+1] = count;
+        count = 0;
+        lo = hi;
+    }
+    return result;
 };
