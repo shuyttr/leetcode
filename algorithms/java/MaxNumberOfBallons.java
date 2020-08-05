88a460ec8b471eb1bc03136a49b1cd0a316933e1
Ari Roffe <aroffe@mail.yu.edu>
2020-08-05 09:42:01 -0500

    10/23 passed
    fix indentation

--- algorithms/java/MaxNumberOfBallons.java
+++ algorithms/java/MaxNumberOfBallons.java
@@ -1,9 +1,13 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
         HashMap<Character, Integer> map = new HashMap<>();
-        for(int i = 0; i < text.length; i++){
+        for(int i = 0; i < text.length(); i++){
             if("balloons".indexOf(text.charAt(i)) > -1){
-                map.put(text.charAt(i), map.get(text.charAt(i)) + 1);
+                if(map.get(text.charAt(i)) != null){
+                    map.put(text.charAt(i), map.get(text.charAt(i)) + 1);
+                } else {
+                    map.put(text.charAt(i), 1);
+                }
             }
         }
         //search thru map and decrease the count at that letter
@@ -12,15 +16,16 @@ class Solution {
         //by a 0, then we return the count
         int hits = 0;
         String balloon = "balloon";
-        for(int i = 0; i < balloon.length; i++){
-            if(map.get(balloon.charAt(i)) > 0){
-                map.get(balloon.charAt(i))--;
-                if(balloon.charAt(i) == 'n'){
-                    hits++;
+        int j = text.length();
+            for(int i = 0; i < balloon.length(); i++){
+                if(map.containsKey(balloon.charAt(i))){
+                    map.put(balloon.charAt(i), map.get(balloon.charAt(i)) - 1);
+                    if(balloon.charAt(i) == 'n'){
+                        hits++;
+                    }
+                } else {
+                    return hits;
                 }
-            } else {
-                return hits;
-            }
         }
         return hits;
     }
