c4414a14af43f577ae3ee8d6b72e0f3ffb77de64
Ari Roffe <aroffe@mail.yu.edu>
2020-08-04 22:38:40 -0500

    logic to only add letter of balloon to map

--- algorithms/java/MaxNumberOfBallons.java
+++ algorithms/java/MaxNumberOfBallons.java
@@ -2,7 +2,9 @@ class Solution {
     public int maxNumberOfBalloons(String text) {
         HashMap<Character, Integer> map = new HashMap<>();
         for(int i = 0; i < text.length; i++){
-            map.put(text.charAt(i), map.get(text.charAt(i)) + 1);
+            if("balloons".indexOf(text.charAt(i)) > -1){
+                map.put(text.charAt(i), map.get(text.charAt(i)) + 1);
+            }
         }
         
     }
