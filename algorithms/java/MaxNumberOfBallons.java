6ae1eba72c1a2575b502b8178a55d42a303e2da5
Ari Roffe <aroffe@mail.yu.edu>
2020-08-05 09:22:00 -0500

    with lookup logic

--- algorithms/java/MaxNumberOfBallons.java
+++ algorithms/java/MaxNumberOfBallons.java
@@ -6,6 +6,22 @@ class Solution {
                 map.put(text.charAt(i), map.get(text.charAt(i)) + 1);
             }
         }
-        
+        //search thru map and decrease the count at that letter
+        //once we hit the last letter 'n', then hit++
+        //once we "search" for a letter and its not there, indicated
+        //by a 0, then we return the count
+        int hits = 0;
+        String balloon = "balloon";
+        for(int i = 0; i < balloon.length; i++){
+            if(map.get(balloon.charAt(i)) > 0){
+                map.get(balloon.charAt(i))--;
+                if(balloon.charAt(i) == 'n'){
+                    hits++;
+                }
+            } else {
+                return hits;
+            }
+        }
+        return hits;
     }
 }
